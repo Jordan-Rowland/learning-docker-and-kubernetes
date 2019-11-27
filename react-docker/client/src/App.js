40561e879bb267185268a36ad8712e6688819a32
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-11-26 16:10:09 -0800

    updates

--- react-docker/client/src/App.js
+++ react-docker/client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            rorschach
+            who watches the watchmen?
         </p>
         <a
           className="App-link"
